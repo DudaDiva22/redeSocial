3b7ecb08f2c44c17a68f2c640d3c1548445419be
DudaDiva22 <eduardacoradin3@gmail.com>
2026-09-09 11:33:28 -0300

    ok

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener( "DOMContentLoade"), ()=>{
+    const LikeBtn = document.querySelector(".like-btn");
+    const postMedia = document.querySelector(".post-media")
+    if (!likeBtn) return;
+
+    const likesCountSpan = likeBtn.querySelector(".likes-count");
+    const bookmarkBtn = document.querySelector(.bookmark-btn);
+
+    let isLiked = false;
+    let baseLikes = 0;
+}
