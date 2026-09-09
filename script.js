89783cd5fe707dcda9dcdbe9d8c563a0b2e671cc
DudaDiva22 <eduardacoradin3@gmail.com>
2026-09-09 11:47:00 -0300

    ok

--- script.js
+++ script.js
@@ -21,6 +21,6 @@ document.addEventListener( "DOMContentLoade"), ()=>{
         if(num >=1000){
             return (num/1000).toFixed(1)+"K";
         }
-        
+        return num.toString();
     }
 }
